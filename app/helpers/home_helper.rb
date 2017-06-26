c2b8e185b54ede73ffc6d7a172369f7324126615
Leandro Macedo <contato@leandromacedo.com>
2017-06-26 23:18:33 +0100

    Create helper to fix count Clients, Member and Skill in home

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+
+    def total_clients
+        @clients.count - 1
+    end
+
+    def total_members
+        @members.count - 1
+    end
+
+    def total_skills
+        @skills.count - 1
+    end
+
 end
